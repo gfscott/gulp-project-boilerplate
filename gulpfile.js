ce3162b1398be5ed8a69384c15c1b1dc2a9dbc97
gfscott <gfscott@users.noreply.github.com>
2018-12-02 15:38:48 -0500

    security updates and slight reformatting

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,36 @@
-var gulp = require('gulp'),
-    rename = require('gulp-rename'),
-    uglify = require('gulp-uglify'),
-    autoprefixer = require('autoprefixer'),
-    cssnano = require('cssnano'),
-    postcss = require('gulp-postcss');
+var gulp = require("gulp"),
+  rename = require("gulp-rename"),
+  uglify = require("gulp-uglify"),
+  autoprefixer = require("autoprefixer"),
+  cssnano = require("cssnano"),
+  postcss = require("gulp-postcss");
 
+gulp.task("default", ["html", "css", "js"]);
 
-gulp.task('default', ['html', 'css', 'js']);
-
-gulp.task('html', function(){
-  gulp.src('./src/**/*.html')
-    .pipe(gulp.dest('./dist'));
+gulp.task("html", function() {
+  gulp.src("./src/**/*.html").pipe(gulp.dest("./dist"));
 });
 
-gulp.task('css', function(){
+gulp.task("css", function() {
+  var postcss_plugins = [autoprefixer({ browsers: [">1%"] }), cssnano()];
 
-  var postcss_plugins = [
-    autoprefixer({browsers: ['>1%']}),
-    cssnano()
-  ];
-
-  gulp.src('./src/css/**/*.css')
+  gulp
+    .src("./src/css/**/*.css")
     .pipe(postcss(postcss_plugins))
-    .pipe(gulp.dest('./dist/css'));
+    .pipe(gulp.dest("./dist/css"));
 });
 
-gulp.task('js', function(){
-  gulp.src('./src/js/**/*.js')
-    .pipe(gulp.dest('./dist/js'))
+gulp.task("js", function() {
+  gulp
+    .src("./src/js/**/*.js")
+    .pipe(gulp.dest("./dist/js"))
     .pipe(uglify())
-    .pipe(rename({extname: '.min.js'}))
-    .pipe(gulp.dest('./dist/js'));
+    .pipe(rename({ extname: ".min.js" }))
+    .pipe(gulp.dest("./dist/js"));
 });
 
-gulp.task('watch', function(){
-  gulp.watch('./src/**/*.html', ['html']);
-  gulp.watch('./src/**/*.css', ['css']);
-  gulp.watch('./src/**/*.js', ['js']);
+gulp.task("watch", function() {
+  gulp.watch("./src/**/*.html", ["html"]);
+  gulp.watch("./src/**/*.css", ["css"]);
+  gulp.watch("./src/**/*.js", ["js"]);
 });
